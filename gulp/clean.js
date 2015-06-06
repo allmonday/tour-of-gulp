1dd5df56b73921ac90e379597f617fc5da2fc416
tangkikodo <allmonday@126.com>
2015-06-07 07:56:32 +0800

    up

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,8 +1,8 @@
 module.exports = function (gulp, $, config) {
 	return function () {
 		gulp.src(['./public'], {read:false})
-		.pipe($.clean({
-			force: true
-		}));
+			.pipe($.clean({
+				force: true
+			}));
 	}
 }
